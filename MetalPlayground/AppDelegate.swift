f55eb5aab9d509f853e314eec4368c9e0af82928
Raheel Ahmad <ahmadraheel@gmail.com>
2020-05-11 22:50:24 -0700

    Initial Commit

---
+++ MetalPlayground/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalPlayground
+//
+//  Created by Raheel Ahmad on 5/11/20.
+//  Copyright © 2020 Raheel Ahmad. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalPlayground/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalPlayground
+//
+//  Created by Raheel Ahmad on 5/11/20.
+//  Copyright © 2020 Raheel Ahmad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
