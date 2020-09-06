8169811ee925775618964ddf5d10b1403e50fe0c
Raheel Ahmad <ahmadraheel@gmail.com>
2020-09-05 17:03:49 -0700

    Uniforms for all Ray matrices

--- MetalPlayground/Scenes/Explorations/Rays.swift
+++ MetalPlayground/Scenes/Explorations/Rays.swift
@@ -15,6 +15,195 @@ struct RaysUniforms {
     var projection: matrix_float4x4
 }
 
+private class RaysConfig: ObservableObject {
+    @Published var cameraPosX: Float = 0
+    @Published var cameraPosY: Float = 0
+    @Published var cameraPosZ: Float = -3
+    @Published var cameraRotX: Float = 0
+    @Published var cameraRotY: Float = 0
+    @Published var cameraRotZ: Float = 0
+
+    @Published var modelPosX: Float = 0
+    @Published var modelPosY: Float = 0
+    @Published var modelPosZ: Float = 0
+    @Published var modelRotX: Float = 0
+    @Published var modelRotY: Float = 0
+    @Published var modelRotZ: Float = 0
+
+    @Published var modelScale: Float = 1
+
+
+    func resetModelPos() {
+        modelPosX = 0
+        modelPosY = 0
+        modelPosZ = -3
+    }
+
+    func resetModelRot() {
+        modelRotX = 0
+        modelRotY = 0
+        modelRotZ = 0
+    }
+
+    func resetModelScale() {
+        modelScale = 1
+    }
+
+    func resetCameraPos() {
+        cameraPosX = 0
+        cameraPosY = 0
+        cameraPosZ = -3
+    }
+
+    func resetCameraRot() {
+        cameraRotX = 0
+        cameraRotY = 0
+        cameraRotZ = 0
+    }
+}
+
+private let raysConfig = RaysConfig()
+
+struct RaysConfigView: View {
+    @EnvironmentObject fileprivate var config: RaysConfig
+
+    private var modelPos: some View {
+        VStack {
+            Text("Model").font(.subheadline)
+            Spacer().frame(height: 10)
+            HStack {
+                Text("Position")
+                Button(action: {
+                    raysConfig.resetModelPos()
+                }, label: { Text("Reset") })
+            }
+            VStack(spacing: 0) {
+                HStack {
+                    Text("x")
+                    Slider(value: $config.modelPosX, in: Float(-20.0)...Float(20.0))
+                        .frame(minWidth: 120)
+                }
+                HStack {
+                    Text("y")
+                    Slider(value: $config.modelPosY, in: Float(-20.0)...Float(20.0))
+                }
+                HStack {
+                    Text("z")
+                    Slider(value: $config.modelPosZ, in: Float(-20.0)...Float(20.0))
+                }
+            }
+        }
+    }
+
+    var modelRot: some View {
+        VStack {
+            HStack {
+                Text("Rotation")
+                Button(action: {
+                    raysConfig.resetModelRot()
+                }, label: { Text("Reset") })
+            }
+            VStack(spacing: 0) {
+                HStack {
+                    Text("x")
+                    Slider(value: $config.modelRotX, in: Float(-3.0)...Float(3.0))
+                        .frame(minWidth: 120)
+                }
+                HStack {
+                    Text("y")
+                    Slider(value: $config.modelRotY, in: Float(-3.0)...Float(3.0))
+                }
+                HStack {
+                    Text("z")
+                    Slider(value: $config.modelRotZ, in: Float(-3.0)...Float(3.0))
+                }
+            }
+        }
+    }
+
+    var modelScale: some View {
+        VStack {
+            HStack {
+                Text("Scale")
+                Button(action: {
+                    raysConfig.resetModelScale()
+                }, label: { Text("Reset") })
+            }
+            VStack(spacing: 0) {
+                Slider(value: $config.modelScale, in: Float(0.0)...Float(1.0))
+                    .frame(minWidth: 120)
+            }
+        }
+    }
+
+    var model: some View {
+        VStack(spacing: 10) {
+            modelPos
+            modelRot
+            modelScale
+        }
+    }
+
+    var camera: some View {
+        VStack {
+            Text("Camera").font(.subheadline)
+            Spacer().frame(height: 10)
+            HStack {
+                Text("Position")
+                Button(action: {
+                    raysConfig.resetCameraPos()
+                }, label: { Text("Reset") })
+            }
+            VStack(spacing: 0) {
+                HStack {
+                    Text("x")
+                    Slider(value: $config.cameraPosX, in: Float(-20.0)...Float(20.0))
+                        .frame(minWidth: 120)
+                }
+                HStack {
+                    Text("y")
+                    Slider(value: $config.cameraPosY, in: Float(-20.0)...Float(20.0))
+                }
+                HStack {
+                    Text("z")
+                    Slider(value: $config.cameraPosZ, in: Float(-20.0)...Float(20.0))
+                }
+            }
+            Spacer().frame(height: 10)
+            HStack {
+                Text("Rotation")
+                Button(action: {
+                    raysConfig.resetCameraRot()
+                }, label: { Text("Reset") })
+            }
+            VStack(spacing: 0) {
+                HStack {
+                    Text("x")
+                    Slider(value: $config.cameraRotX, in: Float(-3.0)...Float(3.0))
+                        .frame(minWidth: 120)
+                }
+                HStack {
+                    Text("y")
+                    Slider(value: $config.cameraRotY, in: Float(-3.0)...Float(3.0))
+                }
+                HStack {
+                    Text("z")
+                    Slider(value: $config.cameraRotZ, in: Float(-3.0)...Float(3.0))
+                }
+            }
+        }
+    }
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            camera
+            model
+
+            Spacer()
+        }
+    }
+}
+
 struct Rays: Scene {
     var name: String { "Rays" }
     var vertexFuncName: String { "rays_vertex" }
@@ -40,15 +229,14 @@ struct Rays: Scene {
     }
 
     var position: float3 = [0,0.3,0]
-    var rotation: float3 = [0, radians_from_degrees(Float(45)),0]
+    var rotation: float3 = [0, radians_from_degrees(Float(0)),0]
     var scale: float3 = [1,1,1]
 
     var modelMatrix: float4x4 {
-        let t = float4x4(translation: position)
-        let r = float4x4(rotation: rotation)
-//        let s = float4x4(scaling: scale)
-        return t * r
-//            * s
+        let t = float4x4(translation: [raysConfig.modelPosX, raysConfig.modelPosY, raysConfig.modelPosZ])
+        let r = float4x4(rotation: [raysConfig.modelRotX, raysConfig.modelRotY, raysConfig.modelRotZ])
+        let s = float4x4(scaling: [raysConfig.modelScale, raysConfig.modelScale, raysConfig.modelScale])
+        return t * r * s
     }
 
     func setUniforms(device: MTLDevice, encoder: MTLRenderCommandEncoder) {
@@ -58,22 +246,21 @@ struct Rays: Scene {
         encoder.setVertexBytes(&u, length: length, index: 1)
     }
 
-    var cameraPos: float3 = [0.0, 0, -3]
-
     var viewMatrix: float4x4 {
-        let t = float4x4(translation: cameraPos)
+        let t = float4x4(translation: [raysConfig.cameraPosX, raysConfig.cameraPosY, raysConfig.cameraPosZ])
+        let r = float4x4(rotation: [raysConfig.cameraRotX, raysConfig.cameraRotY, raysConfig.cameraRotZ])
 
-        return t.inverse
+        let mat = t * r
+
+        return mat.inverse
     }
 
     func mesh(device: MTLDevice) -> MTKMesh? {
         let allocator = MTKMeshBufferAllocator(device: device)
         let size: Float = 1
         let mdlMesh = MDLMesh(
-            capsuleWithExtent: [size,size,size],
-            cylinderSegments: vector_uint2(20,20),
-            hemisphereSegments: 30, inwardNormals: false,
-            geometryType: .triangles,
+            cylinderWithExtent: [size,size*10,size], segments: vector_uint2(80,80), inwardNormals: false,
+            topCap: false, bottomCap: false, geometryType: .triangles,
             allocator: allocator
         )
         do {
@@ -85,7 +272,12 @@ struct Rays: Scene {
     }
 
     var view: NSView? {
-        nil
-//        NSHostingView(rootView: <#T##_#>)
+        NSHostingView(rootView: RaysConfigView().environmentObject(raysConfig))
+    }
+}
+
+struct Rays_Previews: PreviewProvider {
+    static var previews: some View {
+        RaysConfigView().environmentObject(raysConfig)
     }
 }
